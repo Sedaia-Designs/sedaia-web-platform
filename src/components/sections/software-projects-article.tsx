deacb0938b02189594b03ef121b08d431a78e775
Sakura Sedaia <email@sakura-sedaia.com>
2026-08-20 07:50:00 -0500

    [Refactor]: Added `open in new tab` attributes to SoftwareProjects article links

--- src/components/sections/software-projects-article.tsx
+++ src/components/sections/software-projects-article.tsx
@@ -8,9 +8,30 @@ export default function SoftwareProjectsArticle() {
       <div class="project-container">
         <h3>Blender Development for Pycharm</h3>
         <div class="router left">
-          <div class="project-link"><a href="https://www.sedaia-designs.org/projects/blender-development">Project Page</a></div>
-          <div class="project-link"><a href="https://gitlab.com/sedaia-designs/blender_pycharm">Source</a></div>
-          <div class="project-link"><a href="https://docs.blender-development.sakura-sedaia.tech/">Documentation</a></div>
+          <div class="project-link">
+            <a href="https://www.sedaia-designs.org/projects/blender-development"
+              rel={"noreferrer noopener"}
+              target={"_blank"}
+            >
+              Project Page
+            </a>
+          </div>
+          <div class="project-link">
+            <a href="https://gitlab.com/sedaia-designs/blender_pycharm"
+              rel={"noreferrer noopener"}
+              target={"_blank"}
+            >
+              Source
+            </a>
+          </div>
+          <div class="project-link">
+            <a href="https://docs.blender-development.sakura-sedaia.tech/"
+              rel={"noreferrer noopener"}
+              target={"_blank"}
+            >
+              Documentation
+            </a>
+          </div>
         </div>
         <p><strong>Blender Development</strong> is a plugin originally developed for PyCharm. The original idea is based on the <a href="https://github.com/JacquesLucke/blender_vscode">Blender Development</a> extension by Jacques Lucke for Visual Studio Code, of which my plugin's core Python runtime is forked from. The plugin is developed in Kotlin, and is heavily integrated into the Intellij Platform SDK, granting it more advanced and integrated features including:</p>
         <ul><li>Managed Blender Installs</li><li>Access to Pycharm's advanced debugging tools</li><li>Python Intellisense Stub installations</li></ul>
@@ -19,9 +40,33 @@ export default function SoftwareProjectsArticle() {
       <div class="project-container">
         <h3 id={"heading-advanced-character-rig"}>Advanced Character Rig</h3>
         <div class="router left">
-          <div class="project-link"><a href="https://www.sedaia-designs.org/projects/sakura-character-rig">Project Page</a></div>
-          <div class="project-link"><a href="https://gitlab.com/sedaia-designs/advanced-character-rig">Source</a></div>
-          <div class="project-link"><a href="https://docs.sakura-sedaia.com">Documentation</a></div>
+          <div class="project-link">
+            <a
+              href="https://www.sedaia-designs.org/projects/sakura-character-rig"
+              rel={"noreferrer noopener"}
+              target={"_blank"}
+            >
+              Project Page
+            </a>
+          </div>
+          <div class="project-link">
+            <a
+              href="https://gitlab.com/sedaia-designs/advanced-character-rig"
+              rel={"noreferrer noopener"}
+              target={"_blank"}
+            >
+              Source
+            </a>
+          </div>
+          <div class="project-link">
+            <a
+              href="https://docs.sakura-sedaia.com"
+              rel={"noreferrer noopener"}
+              target={"_blank"}
+            >
+              Documentation
+            </a>
+          </div>
         </div>
         <p><strong>Sakura Advanced Character Rig (SACR)</strong> is a Blender rig and toolkit for creating Minecraft-style character renders. The project brings its independently released components together in one repository, including:</p>
         <ul><li>Character rig releases, source assets, and supporting files</li><li>Sakura Rig Utilities for rig and skin management workflows</li><li>Reusable Blender scripts for specialized, one-off tasks</li></ul>
