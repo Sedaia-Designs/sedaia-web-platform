ffa7aaf32e22d3eb22f3508a1cc37d056e33b1f1
SakuraSedaia <email@sakura-sedaia.com>
2026-09-24 15:12:23 -0500

    [Docs: kdoc] Added Documentation noting the main API app Entry Point

--- apps/api/src/main/kotlin/org/sedaiadesign/api/main.kt
+++ apps/api/src/main/kotlin/org/sedaiadesign/api/main.kt
@@ -2,6 +2,12 @@ package org.sedaiadesign.api
 
 import io.ktor.server.netty.EngineMain
 
+/**
+ * Main entry point of the application, invoked when the server starts.
+ * Delegates to Ktor's EngineMain to start the server engine.
+ *
+ * @param args Command-line arguments passed to the application.
+ */
 fun main(args: Array<String>) {
   EngineMain.main(args)
 }
